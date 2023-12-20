a46617f5d7afc96d2bc746c4f89923a43631f02e
himanshu474 <60884300+himanshu474@users.noreply.github.com>
2023-12-20 12:56:40 +0000

    commit

--- 3-DIGITAL_CLOCK/digital.js
+++ 3-DIGITAL_CLOCK/digital.js
@@ -3,8 +3,4 @@ const clock=document.getElementById('clock');
 setInterval(() => {
     let date= new Date(); 
     clock.innerHTML=date.toLocaleTimeString();
-<<<<<<< HEAD
 }, 2000);
-=======
-}, 2000);
->>>>>>> b9184a9 (container config done)
